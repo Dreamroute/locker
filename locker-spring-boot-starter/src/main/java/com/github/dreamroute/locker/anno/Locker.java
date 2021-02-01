02775592a313c0938d99add0e73b4568ddf30bac
w.dehai <342252328@qq.com>
2021-02-01 15:41:07 +0800

    fix: 优化一些细节

--- locker-spring-boot-starter/src/main/java/com/github/dreamroute/locker/anno/Locker.java
+++ locker-spring-boot-starter/src/main/java/com/github/dreamroute/locker/anno/Locker.java
@@ -7,7 +7,7 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * 乐观锁标记，被此注解标记的update方法将被插件拦截改写slq
+ * 乐观锁标记，被此注解标记的update方法将被插件拦截改写sql
  *
  * @author w.dehi
  */
